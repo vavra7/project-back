bc3ac5461f2bc2e380b7ac17bdc5b1faf57140d4
Tomáš Vavřinka <email@tomasvavrinka.cz>
2020-05-18 17:40:00 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import { RegisterResolver } from "./modules/user/Register";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
 
 const main = async () => {
   await createConnection();
@@ -11,12 +15,38 @@ const main = async () => {
   const schema = await buildSchema({
     resolvers: [RegisterResolver],
   });
-  const apolloServer = new ApolloServer({ schema });
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }: any) => ({ req }),
+  });
   const app = Express();
+  const RedisStore = connectRedis(session);
 
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+  );
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: "qid",
+      secret: "asdfadsfadsf",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
   apolloServer.applyMiddleware({ app });
-  app.listen(4050, () => {
-    console.log("Server is up and running on http://localhost:4050/graphql");
+  app.listen(4000, () => {
+    console.log("Server is up and running on http://localhost:4000/graphql");
   });
 };
 

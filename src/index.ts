d4e293ec8a94377658d1c1987710d6193277a29d
Tomas Vavrinka <tomas.vavrinka@seznam.cz>
2020-05-19 15:00:00 +0200

    middlewares

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,9 @@ const main = async () => {
 
   const schema = await buildSchema({
     resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    authChecker: ({ context: { req } }) => {
+      return !!req.session.userId;
+    },
   });
   const apolloServer = new ApolloServer({
     schema,

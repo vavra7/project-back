2ba8a956c61273edddfc06a2953d318a48b7dff9
Tomas Vavrinka <tomas.vavrinka@seznam.cz>
2020-05-20 15:10:18 +0200

    reset password

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,16 @@ import { ApolloServer } from "apollo-server-express";
 import Express from "express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
-import { RegisterResolver } from "./modules/user/Register";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { redis } from "./redis";
 import cors from "cors";
-import { LoginResolver } from "./modules/user/Login";
-import { MeResolver } from "./modules/user/Me";
-import { sendEmail } from "./modules/utils/sendEmail";
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver, LoginResolver, MeResolver],
+    resolvers: [__dirname + "/modules/**/*.ts"],
     authChecker: ({ context: { req } }) => {
       return !!req.session.userId;
     },
---
+++ src/modules/user/ConfirmUser.ts
@@ -0,0 +1,23 @@
+import { Resolver, Mutation, Arg } from "type-graphql";
+import { redis } from "../../redis";
+import { User } from "../../entity/User";
+import { confirmUserPrefix } from "../constants/redisPrefixes";
+
+@Resolver()
+export class ConfirmUserResolver {
+  @Mutation(() => Boolean)
+  async confirmUser(
+    @Arg("token") token: string,
+  ): Promise<boolean> {
+    const userId = await redis.get(token)
+
+    if (!userId) {
+      return false;
+    }
+
+    await User.update({id: parseInt(userId)}, {confirmed: true})
+    await redis.del(confirmUserPrefix + token);
+
+    return true
+  }
+}
